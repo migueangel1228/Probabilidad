f033e2d7154644943009c47346c7ea5429d945e7
Mianparo <147523486+migueangel1228@users.noreply.github.com>
2025-07-30 10:35:16 -0500

    Add files via upload

---
+++ Probabilidad/introBasesDatos.R
@@ -0,0 +1,15 @@
+# Programa 1
+# Intro Bases de Datos
+
+
+data(iris) # data iris
+head(iris) # Muestra los primeros registros de la data
+
+
+
+
+summary(iris$Sepal.Length)
+
+iris[1,1] = 6.2
+
+head(iris) # Muestra laActualizacion de el dato fila 1 columna 1
